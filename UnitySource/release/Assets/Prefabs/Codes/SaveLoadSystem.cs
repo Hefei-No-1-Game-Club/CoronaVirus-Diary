112d517ec873bc27c0b9a19f20112e304c0e46cb
lvwenzhuo2003 <lvwenzhuo2003@126.com>
2020-10-09 12:41:15 +0800

    Cleanup

---
+++ UnitySource/release/Assets/Prefabs/Codes/SaveLoadSystem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SaveLoadSystem : MonoBehaviour
+{
+
+    public void save() {
+        PlayerPrefs.SetInt("current progress", SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void load() {
+        int progress = PlayerPrefs.GetInt("current progress", 1);
+        SceneManager.LoadScene(progress);
+    }
+
+}
